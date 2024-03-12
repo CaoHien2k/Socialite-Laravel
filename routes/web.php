317b0f37412234f83b5ad30d740534ce71edf87e
HienCTT <hienctt.nta@gmail.com>
2024-03-12 10:16:20 +0700

    laravel socialate

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Google login
+Route::get('login/google', [LoginController::class, 'redirectToGoogle'])->name('login.google');
+Route::get('login/google/callback', [LoginController::class, 'handleGoogleCallBack']);
+
+//Facebook login
+//Route::get('login/facebook', [LoginController::class, 'redirectToFacebook'])->name('login.facebook');
+//Route::get('login/facebook/callback', [LoginController::class, 'handleFacebookCallBack']);
+
+//GitHub login
+Route::get('login/github', [LoginController::class, 'redirectToGithub'])->name('login.github');
+Route::get('login/github/callback', [LoginController::class, 'handleGithubCallBack']);
